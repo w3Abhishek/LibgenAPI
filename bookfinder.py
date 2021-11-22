e3daeb1ff59f3f4dc120074fb76e73d4833871a6
Abhishek Verma <abhishekverma3124@gmail.com>
2021-11-23 02:25:20 +0530

    Adding files initially

--- bookfinder.py
+++ bookfinder.py
@@ -1,7 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import random
-import html
+
+from requests.api import head
+# http://libgen.st/search.php?req=Python&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def
+
+libraryDomain = 'http://libgen.st'
 user_agent_list = [
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
@@ -11,46 +15,60 @@ user_agent_list = [
 ]
 user_agent = random.choice(user_agent_list)
 headers = {'User-Agent': user_agent}
-proxyDomain = 'http://libgen.st/'
-downDomain = "http://library.lol/main/"
+
 def search(query):
-  queryURL = '%ssearch.php?req=%s&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def'%(proxyDomain,query)
-  response = requests.get(queryURL,headers=headers)
-  headDoc = BeautifulSoup(response.text, 'html.parser')
-  result = headDoc.find_all("a", attrs={"id":True, "title":True, "href":True})
-  count = 0
-  searchres = "Results: \n\n"
-  apiResponse = {}
-  for i in result:
-    pageLink = downDomain + i['href'].split('md5=')[1]
-    srequest = requests.get(pageLink, headers = headers)
-    srequest.encoding = 'utf-8'
-    nisoup = BeautifulSoup(srequest.text, 'html.parser')
-    bookDetails = nisoup.find_all("p")
-    pageres = str(nisoup.h2)
-    bookName = nisoup.h1.text
-    fileLink = pageres.split('"')[1]
-    bookcover = "http://library.lol/" + str(nisoup.img['src'])
-    altL = nisoup.find_all("a")
+    bookDetails = {}
+    url = libraryDomain + '/search.php?req=' + query + '&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def'
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    results = soup.find_all('tr', attrs={"valign":True, "bgcolor": True})
     try:
-      authorName = bookDetails[0].text.replace('Author(s): ', '')
+        for count, result in enumerate(results):
+            if count != 0:
+                bookDetail = result.find_all('td')
+                bookAuthor = bookDetail[1].find('a')
+                # bookISBN = str(bookDetail[2].find('a'))
+                bookTitle = bookDetail[2].find('a')
+                bookPublisher = bookDetail[3].text
+                bookID = bookDetail[0].text
+                bookAuthorName = bookAuthor.text
+                bookAuthorURL = bookAuthor['href']
+                # bookISBN = bookISBN.split('<i>')[2].split('</i>')[0]
+                # print(bookISBN)
+                bookYear = bookDetail[4].text
+                bookPages = bookDetail[5].text
+                bookLanguage = bookDetail[6].text
+                bookSize = bookDetail[7].text
+                bookExtension = bookDetail[8].text
+                md5 = bookTitle['href'].split('=')[1]
+                bookDetails[count] = {
+                    'bookID': bookID,
+                    'bookTitle': bookTitle.text,
+                    'author': bookAuthorName,
+                    'authorLink': bookAuthorURL,
+                    # 'bookISBN':bookISBN,
+                    'publisher': bookPublisher,
+                    'year': bookYear,
+                    'pages': bookPages,
+                    'lang':bookLanguage,
+                    'size': bookSize,
+                    'fileFormat':bookExtension,
+                    'md5':md5
+                    }
+            else:
+                pass
+            count += 1
+        return bookDetails
     except:
-      authorName = "Anonymous"
-    apiResponse[count] = {}
-    altL = nisoup.find_all("a")
-    c = 0
-    altLinks = []
-    try:
-      for i in range(1,6):
-        altLinks[count] = altL[i]['href']
-        c += 1
-    except:
-      altLinks = []
-    apiResponse[count]['altlinks'] = altLinks
-    apiResponse[count]['bookname'] = str(bookName)
-    apiResponse[count]['filelink'] = str(fileLink)
-    apiResponse[count]['authorname'] = str(authorName)
-    apiResponse[count]['cover'] = str(bookcover)
-    count += 1
-  apiResponse = {'count':count, 'results':apiResponse}
-  return apiResponse
+        bookDetails = {}
+        return bookDetails
+
+def getBook(query):
+    md5 = query
+    url = 'http://library.lol/main/' + md5
+    r = requests.get(url, headers=headers)
+    nisoup = BeautifulSoup(r.text, 'html.parser')
+    allLinks = nisoup.find_all('a')
+    mainLink = allLinks[0]['href']
+    result = {'filelink':mainLink, 'altLinks':[allLinks[1]['href'], allLinks[2]['href'], allLinks[3]['href'], allLinks[4]['href']]}
+    return result
